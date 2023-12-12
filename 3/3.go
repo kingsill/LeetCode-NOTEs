6d563f09f40fcdbfef199e73b4a6d50e368943e9
wangzilong <wang2001zilong@126.com>
2023-12-12 21:39:15 +0800

    12.12 5

--- 3/3.go
+++ 3/3.go
@@ -1,1 +1,13 @@
-package _
+package main
+
+import "fmt"
+
+func main() {
+	s := "abcabcbb"
+	c := lengthOfLongestSubstring(s)
+	fmt.Println(c)
+}
+
+func lengthOfLongestSubstring(s string) int {
+	return 1
+}
