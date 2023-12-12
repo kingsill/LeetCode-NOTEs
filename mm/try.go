753510259ee87bd4826aeb2af52c2a41a5fc9f6c
wzl <wang2001zilong@126.com>
2023-12-12 11:24:28 +0800

    12.12

--- mm/try.go
+++ mm/try.go
@@ -12,37 +12,50 @@ type ListNode struct {
 func main() {
 	l := CreateList(13)
 	fmt.Printf("l: %v\n", l)
-	ReadList(*l)
+	// ReadList(*l)
+	RecurReadList(l)
+	RecurReadList(l.Next)
 	// fmt.Println(l.Next.Val)
 }
 
-// 尝试建立链表
+// 尝试建立有头节点的链表，关键在于赋值给middle.next
 func CreateList(nums int) *ListNode {
-	Head := new(ListNode) //这是一整个链表
+	Head := new(ListNode) //这代表一整个链表，并通过这里的头节点进行标注，方便该链表的引用
 
 	middle := Head //middle视作Head链表的中间节点，其一直改变
 
 	for nums > 0 {
-		middle.Next = &ListNode{Val: nums % 10}
+		middle.Next = &ListNode{Val: nums % 10} //头节点赋值方法
 		fmt.Printf("middle.Val: %v\n", middle.Val)
 		middle = middle.Next
 		nums /= 10
 	}
-
 	return Head
 }
 
-// 尝试便利读取链表
+// 尝试遍历读取链表
 func ReadList(L ListNode) {
-	middle := *L.Next
-	fmt.Printf("middle: %v\n", middle)
-	fmt.Println(middle.Val)
-	fmt.Println(middle.Next.Val)
-	// for middle.Next != nil {
-	// 	v := L.Val
-	// 	fmt.Printf("v: %v\n", v)
-	// 	// 	// fmt.Printf("L: %v\n", L)
-	// 	middle = *middle.Next
-	// 	// 	// fmt.Printf("L: %v\n", L)
-	// }
+
+	middle := L //将头节点赋予这里的中间节点middle
+
+	//循环读取链表的内容
+	for middle.Next != nil {
+		v := middle.Next.Val //由于我们这里判断的是本身节点是否为空，所以在输出时使用下一节点的值进行输出，避免错过某个值
+		fmt.Printf("v: %v\n", v)
+		// 	// fmt.Printf("L: %v\n", L)
+		middle = *middle.Next
+		// 	// fmt.Printf("L: %v\n", L)
+	}
+}
+
+// 尝试递归读取链表
+func RecurReadList(L *ListNode) {
+
+	fmt.Printf("L.Val: %v\n", L.Val) //打印出此节点中的Val
+
+	//如果本结点的指针不为空，即还有下一个节点，继续读取
+	if L.Next != nil {
+		RecurReadList(L.Next) //将下个节点的指针传入
+	}
+	//如果运行到这里，说明指针为空，函数也就到此结束了
 }
