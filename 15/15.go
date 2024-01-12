55a5ac979a648a4f0cee7ca5a9283932f0e08f2c
wang2fromWSL <wang2001zilong@126.com>
2024-01-12 22:54:52 +0800

    1.12 1

--- 15/15.go
+++ 15/15.go
@@ -6,15 +6,16 @@ import (
 )
 
 func main() {
-	nums := []int{1, -1, 0, -1, 1}
+	nums := []int{-1, 0, 1, 2, -1, -4, -2, -3, 3, 0, 4}
 	c := threeSum(nums)
 	fmt.Printf("c: %v\n", c)
 
 }
 
+// 得到结果后再去重 失败,超出输出限制
 func threeSum(nums []int) [][]int {
 	L := len(nums)
-
+	
 	var intT string
 
 	result := [][]int{}
@@ -42,7 +43,8 @@ func threeSum(nums []int) [][]int {
 					slices.Sort(order)
 
 					for _, v := range order {
-						intT = fmt.Sprintf(intT, string(rune(v)))
+						intT = fmt.Sprintf(intT, v)
+						fmt.Println(intT)
 					}
 
 					if _, cont := G[intT]; !cont {
@@ -58,3 +60,66 @@ func threeSum(nums []int) [][]int {
 	// fmt.Print(table)
 	return result
 }
+
+// 双指针法
+/*func threeSum(nums []int) [][]int {
+
+	slices.Sort(nums)
+	L := len(nums)
+
+	result := [][]int{}
+
+	for left := 0; left < L; left++ {
+		for middle := left + 1; middle < L; middle++ {
+			for right := middle + 1; right < L; right++ {
+				if nums[left]+nums[middle]+nums[right] == 0 {
+					result = append(result, []int{nums[left], nums[middle], nums[right]})
+				}
+
+			}
+		}
+	}
+
+	fmt.Println(nums)
+
+	return nil
+}*/
+
+// 官方解题
+/*func threeSum(nums []int) [][]int {
+	n := len(nums)
+	sort.Ints(nums)
+	ans := make([][]int, 0)
+
+	// 枚举 first
+	for first := 0; first < n; first++ {
+		// 需要和上一次枚举的数不相同
+		if first > 0 && nums[first] == nums[first-1] {
+			continue
+		}
+		// third 对应的指针初始指向数组的最右端
+		third := n - 1
+		target := -1 * nums[first]
+		// 枚举 second
+		for second := first + 1; second < n; second++ {
+			// 需要和上一次枚举的数不相同
+			if second > first+1 && nums[second] == nums[second-1] {
+				continue
+			}
+			// 需要保证 second 的指针在 third 的指针的左侧
+			for second < third && nums[second]+nums[third] > target {
+				third--
+			}
+			// 如果指针重合，随着 second 后续的增加
+			// 就不会有满足 first+second+third=0 并且 second<third 的 third 了，可以退出循环
+			if second == third {
+				break
+			}
+			if nums[second]+nums[third] == target {
+				ans = append(ans, []int{nums[first], nums[second], nums[third]})
+			}
+		}
+	}
+	return ans
+}
+*/
--- 18/18.go
+++ 18/18.go
@@ -1,1 +1,50 @@
-package _8
+package main
+
+import (
+	"fmt"
+	"slices"
+)
+
+func main() {
+	nums := []int{-2, -1, -1, 1, 1, 2, 2}
+	fmt.Println(fourSum(nums, 0))
+}
+
+func fourSum(nums []int, target int) [][]int {
+	slices.Sort(nums)
+	L := len(nums)
+
+	fourth := L - 1
+
+	record := [][]int{}
+
+	for first := 0; first < L; first++ {
+		if first > 1 && nums[first] == nums[first-1] && nums[first] == nums[first-2] {
+			continue
+		}
+		for second := first + 1; second < L; second++ {
+			if second > 1 && nums[second] == nums[second-1] && nums[second] == nums[second-2] {
+				continue
+			}
+			for third := second + 1; third < L; third++ {
+				if third > 2 && nums[third] == nums[third-1] && nums[third] == nums[third-2] {
+					continue
+				}
+				for nums[first]+nums[second]+nums[third]+nums[fourth] > target && third < fourth {
+					fourth--
+				}
+				if fourth == third {
+					break
+				}
+				if nums[first]+nums[second]+nums[third]+nums[fourth] == target {
+					record = append(record, []int{nums[first], nums[second], nums[third], nums[fourth]})
+				}
+
+			}
+			if fourth == second+1 {
+				break
+			}
+		}
+	}
+	return record
+}
