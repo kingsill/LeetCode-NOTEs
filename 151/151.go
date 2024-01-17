8cf2a348aabd9ee2668e83823acea9e0769162ed
wangzilong <wang2001zilong@126.com>
2024-01-17 23:25:53 +0800

    1.17 55

--- 151/151.go
+++ 151/151.go
@@ -9,7 +9,7 @@ func main() {
 	fmt.Println(s)
 }
 
-func reverseWords(s string) string {
+/*func reverseWords(s string) string {
 	WordList := []string{}
 	left, right := 0, 0
 	L := len(s)
@@ -18,6 +18,7 @@ func reverseWords(s string) string {
 	for i, i2 := range s {
 		//去除重复的空格
 		if i > 0 && s[i-1] == ' ' && i2 == ' ' {
+			left++
 			continue
 		}
 
@@ -37,7 +38,40 @@ func reverseWords(s string) string {
 	S := WordList[0]
 	for i, s2 := range WordList {
 		if i != 0 {
+			S = s2 + " " + S
+		}
+	}
+	return S
+}*/
 
+func reverseWords(s string) string {
+	WordList := []string{}
+	left := 0
+	L := len(s)
+	//fmt.Println(L)
+
+	for i, i2 := range s {
+		//去除重复的空格
+		if i > 0 && s[i-1] == ' ' && i2 == ' ' {
+			left++
+			continue
+		}
+
+		//不为空格时右指针移动，并且如果最后一位也为字母时添加到存储表
+		if i2 != ' ' {
+			if i == L-1 {
+				WordList = append(WordList, s[left:i+1])
+			}
+		} else { //当为空格时，将当前的左右指针对应的单词放入存储表，同时给左指针赋值
+			if i != 0 {
+				WordList = append(WordList, s[left:i])
+			}
+			left = i + 1
+		}
+	}
+	S := WordList[0]
+	for i, s2 := range WordList {
+		if i != 0 {
 			S = s2 + " " + S
 		}
 	}
